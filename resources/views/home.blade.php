b665249223ad13dd6b16a50043f66491face4aef
agnesvarga1 <agikolyok@gmail.com>
2024-03-14 17:06:17 +0100

    cards created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,26 @@
-@extends('layouts.app');
+@extends('layouts.app')
 @section('main')
-<h1>Collection of films</h1>
+<h1 class="text-center text-primary">Collection of films</h1>
 {{-- {{dd($movies)}} --}}
+
+<div class="row">
+    @foreach ($movies as $film )
+    <div class="col-lg-3 col-md-6 m-3">
+
+      <div class="card m-3">
+        <div class="card-body">
+          <h4 class="card-title">{{$film['title']}}</h4>
+          <h6 class="card-title"><span class="fw-light">Titolo originale: </span>{{$film['original_title']}}</h6>
+          <p class="card-text">
+            {{$film["nationality"]}} | {{$film["date"]}} | Vote: {{$film["vote"]}}
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+    @endforeach
+
+  </div>
+
 @endsection
