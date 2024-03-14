bfed40fc41dc20d41cddb49bca91555b31b53dfa
agnesvarga1 <agikolyok@gmail.com>
2024-03-14 16:41:51 +0100

    setup db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/js/app.js')
-    <title>@yield("title")</title>
-</head>
-<body>
- <h1 class="text-center text-success">My Template</h1>
-</body>
-</html>
+@extends('layouts.app');
+@section('main')
+<h1>Collection of films</h1>
+{{-- {{dd($movies)}} --}}
+@endsection
